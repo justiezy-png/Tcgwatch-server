d8ab98a532b2f85c048fe9085dc30cde6495e42b
justiezy-png <justiezy@gmail.com>
2026-05-09 19:06:39 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 3001;
 const CORS_HEADERS = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+  "Access-Control-Allow-Headers": "*",
+  "Access-Control-Max-Age": "86400",
+  "Content-Type": "application/json",
+};
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
   "Content-Type": "application/json",
 };
